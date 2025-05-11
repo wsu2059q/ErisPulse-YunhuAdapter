3f95cb56a94bff1e563f0968f555bb00147b7796
WSu2059 <zlb10210722@gmail.com>
2025-05-11 22:22:53 +0800

    BotFollow update

---
+++ BotFollowed/__init__.py
@@ -0,0 +1,17 @@
+moduleInfo = {
+    "meta": {
+        "name": "BotFollowed",
+        "version": "1.0.0",
+        "description": "异步云湖适配器 - 关注机器人事件处理模块",
+        "author": "WSu2059",
+        "license": "MIT",
+        "homepage": "https://github.com/wsu2059q/ErisPulse-YunhuAdapter"
+    },
+    "dependencies": {
+        "requires": ["AsyncServer"],
+        "optional": [],
+        "pip": []
+    }
+}
+
+from .Core import Main
